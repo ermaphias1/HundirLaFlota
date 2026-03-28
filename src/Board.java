0d71665e715508cf5007dff02b4cfc0b0edbdf5e
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-03-28 15:29:56 +0100

    commit segundo

--- src/Board.java
+++ src/Board.java
@@ -7,8 +7,7 @@ public class Board {
     private int columnas=5;
     private Celda [][] tablero;
     private Ship[] flota=new Ship[6];
-
-
+    Coordenadas posicion;
     public void inicializarFlota() {
         int[] tamanos = { 3, 2, 2, 1, 1, 1};
         for (int i = 0; i < tamanos.length; i++) {
@@ -16,47 +15,91 @@ public class Board {
         }
     }
 
-
     public void colocarBarcos() {
-
         for (int contadorDeBarcos = 0; contadorDeBarcos < flota.length; contadorDeBarcos++) {
             Coordenadas posicion = colocarBarcosAleatorios();
-            if (posicion.getOrientacion() == 0) {
-                if (posicion.getVertical() - flota[contadorDeBarcos].getTamanio() > 0) {
-                    for (int j = posicion.getVertical(); j > posicion.getVertical() - flota[contadorDeBarcos].getTamanio(); j--) {
-                        tablero[j][posicion.getHorizontal()].setBarco(flota[contadorDeBarcos]);
-                    }
-
-                } else if (posicion.getVertical() + flota[contadorDeBarcos].getTamanio() > filas) {
-                    for (int j = posicion.getVertical(); j < posicion.getVertical() + flota[contadorDeBarcos].getTamanio(); j++) {
-                         tablero[j][posicion.getHorizontal()].setBarco(flota[contadorDeBarcos]);
-                    }
-
-
-                }
-                if (posicion.getOrientacion() == 1) {
-                    if (posicion.getHorizontal() - flota[i].getTamanio() > 0) {
-                        for (int j = posicion.getHorizontal(); j < posicion.getHorizontal() + flota[i].getTamanio(); j++) {
-                            tablero[i][j].isOcupada();
-                        }
-                    }
-                }
-
+            if(sePuedeColocar(posicion,flota[contadorDeBarcos])){
 
             }
 
         }
+            if (posicion.getOrientacion() == 0) {
+                if (posicion.getVertical() - flota[contadorDeBarcos].getTamanio() > 0 && ) {
+                    for (int j = posicion.getVertical(); j > posicion.getVertical() - flota[contadorDeBarcos].getTamanio(); j--) {
+                        if (!tablero[j][posicion.getHorizontal].isVaixell)
 
-    } 
+                    }
+                    for (int j = posicion.getVertical(); j > posicion.getVertical() - flota[contadorDeBarcos].getTamanio(); j--) {
+                       tablero[j][posicion.getHorizontal()].setBarco(flota[contadorDeBarcos]);
+                    }
 
 
-        public Coordenadas colocarBarcosAleatorios () {
+                } else if (posicion.getVertical() + flota[contadorDeBarcos].getTamanio() > filas ){
+                    for (int j = posicion.getVertical(); j < posicion.getVertical() + flota[contadorDeBarcos].getTamanio(); j++) {
+                         tablero[j][posicion.getHorizontal()].setBarco(flota[contadorDeBarcos]);
+                    }
+                }
+
+                if (posicion.getOrientacion() == 1) {
+                    if (posicion.getHorizontal() - flota[contadorDeBarcos].getTamanio() > 0) {
+                        for (int j = posicion.getHorizontal(); j > posicion.getHorizontal() - flota[contadorDeBarcos].getTamanio(); j--) {
+                            tablero[posicion.getHorizontal()][j].setBarco(flota[contadorDeBarcos]);
+                        }
+
+                    } else if (posicion.getHorizontal() + flota[contadorDeBarcos].getTamanio() > filas) {
+                        for (int j = posicion.getHorizontal(); j < posicion.getHorizontal() + flota[contadorDeBarcos].getTamanio(); j++) {
+                            tablero[j][posicion.getHorizontal()].setBarco(flota[contadorDeBarcos]);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    public boolean sePuedeColocar(Coordenadas pos, int tamanio) {
+        int fila = pos.getVertical();
+        int col = pos.getHorizontal();
+        int ori = pos.getOrientacion();
+        if (ori==0){
+            if(fila-tamanio+1<0){
+                return false;
+            }
+            for (int j = fila; j > fila - tamanio ; j-- ) {
+                if (tablero[j][col].isVaixell)
+                    return false;
+            }
+            return true;
+        } else if (ori==1){
+            if(fila+tamanio>filas){
+                return false;
+            }
+            for (int j = fila; j < fila + tamanio ; j++ ) {
+                if (tablero[j][col].isVaixell)
+                    return false;
+            }
+            return true;
+
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+        public Coordenadas dameCoordenadasAleatorias () {
             int filaAleatoria = rnd.nextInt(filas);
             int colAleatoria = rnd.nextInt(columnas);
             int orientacion = rnd.nextInt(2);
             Coordenadas coordenadasAleatorias = new Coordenadas(filaAleatoria, colAleatoria, orientacion);
             return coordenadasAleatorias;
+
         }
 
 
     } 
+
