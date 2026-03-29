61035ed6d4ac6ca2c667b243594dc20e55d35468
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-03-29 19:32:36 +0200

    commit segundo

---
+++ src/HundirLaFlota.java
@@ -0,0 +1,12 @@
+public class HundirLaFlota {
+
+        public static void main(String[] args) {
+            Board tablero=new Board();
+            tablero.inicializarFlota();
+
+
+            System.out.println(tablero.dameCoordenadasAleatorias());
+        }
+    }
+
+
